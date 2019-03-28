635b610e742b1aed6877ed5e1517b9ec8a2e626d
Jerome Le Saux <j.lesaux@axione.fr>
2019-03-28 17:03:49 +0100

    fix: fix offset to save file and directory entry in dsk.

--- dsk.go
+++ dsk.go
@@ -444,11 +444,11 @@ func (d *DSK) GetPosData(track, sect uint8, SectPhysique bool) uint16 {
 	var SizeByte uint16
 	var Pos uint16
 	var t,s uint8
-	Pos += 256
+	//Pos += 256
 
 	//fmt.Fprintf(os.Stdout,"Track:%d,Secteur:%d\n",track,sect)
 	for t = 0; t <= track; t++ {
-		Pos += 256
+		//Pos += 256
 		for s = 0; s < tr.NbSect; s++ {
 			if t == track {
 				if (tr.Sect[s].R == sect && SectPhysique) || ( s == sect && !SectPhysique ) {
