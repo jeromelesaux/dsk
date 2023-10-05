2af2fb933be06eae3220dd0e3620e756bf2912a3
Jerome Le Saux <jerome.lesaux@treezor.com>
2023-10-05 17:31:53 +0200

    add: add first test for hfe

--- hfe/hfe.go
+++ hfe/hfe.go
@@ -1,5 +1,15 @@
 package hfe
 
+import (
+	"encoding/binary"
+	"errors"
+	"io"
+)
+
+var (
+	NoHFEFormatFound = errors.New("no hfe format found")
+)
+
 type PicFileFormatHeader struct {
 	HeaderSignature     [8]uint8
 	FormatRevision      uint8
@@ -18,6 +28,8 @@ type PicFileFormatHeader struct {
 	Track0s1Altencoding TrackEncoding
 	Track0s1Encoding    TrackEncoding
 }
+
+const Signature = "HXCPICFE"
 
 type FloppyInterfaceMode = uint8
 
@@ -52,4 +64,57 @@ const (
 type PicTrack struct {
 	Offset   uint16
 	TrackLen uint16
+}
+
+type HFE struct {
+	Header PicFileFormatHeader
+	Tracks []PicTrack
+	Size   uint
+}
+
+type uFloppyBit struct {
+	data    byte
+	weak    byte
+	strong  byte
+	mark    byte
+	index   byte
+	garbage [3]byte
+	content byte
+}
+
+type FloppyBit struct {
+	cell     *uFloppyBit
+	position uint16
+	next     *FloppyBit
+}
+
+func ReadHeader(r io.Reader) (PicFileFormatHeader, error) {
+	h := PicFileFormatHeader{}
+	err := binary.Read(r, binary.LittleEndian, &h)
+	if err != nil {
+		return PicFileFormatHeader{}, err
+	}
+
+	if string(h.HeaderSignature[:]) != Signature {
+		return PicFileFormatHeader{}, NoHFEFormatFound
+	}
+
+	return h, nil
+}
+
+func Read(r io.Reader) (HFE, error) {
+	h := HFE{}
+	var err error
+	h.Header, err = ReadHeader(r)
+	if err != nil {
+		return HFE{}, err
+	}
+	h.Tracks = make([]PicTrack, h.Header.NbTracks)
+	for i := 0; i < int(h.Header.NbTracks); i++ {
+		err := binary.Read(r, binary.LittleEndian, &h.Tracks[i])
+		if err != nil {
+			return HFE{}, err
+		}
+	}
+	return h, nil
 }
---
+++ hfe/hfe_test.go
@@ -0,0 +1,31 @@
+package hfe_test
+
+import (
+	"os"
+	"testing"
+
+	"github.com/jeromelesaux/dsk/hfe"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+)
+
+func TestReadHfe(t *testing.T) {
+	t.Run("read_header", func(t *testing.T) {
+		f, err := os.Open("../testdata/fighter_bomber.hfe")
+		require.NoError(t, err)
+
+		h, err := hfe.ReadHeader(f)
+		assert.NoError(t, err)
+		assert.NotEmpty(t, h)
+	})
+
+	t.Run("read_hfe", func(t *testing.T) {
+		f, err := os.Open("../testdata/fighter_bomber.hfe")
+		require.NoError(t, err)
+
+		h, err := hfe.Read(f)
+		assert.NoError(t, err)
+		assert.NotEmpty(t, h)
+	})
+
+}
